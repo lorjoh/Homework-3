d606efd82c851f84b26b98df5acaf1e71437de39
SAMIA <68030713+lorjoh@users.noreply.github.com>
2020-08-27 19:57:37 -0400

    working update

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Homework-3
+# Password Generator (Homework-3)
+  For the var LowerCase = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+  I used the unicode number to represent the letters and the number of letters for its length.
+
+ For the var UpperCase = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
+  I used the unicode number to represent the letters and the number of letters for its length.
+  
+  For the var number = ["0,1,2,3,4,5,6,7,8,9"];
+  I used the unicode number to represent the numbers and the total index for its length.
+  
+  I used var instead of const. to allow for update and changes. 
+  
+  The block bracket used for arrays did not allow for the symbols to recognised as individual symbols rather it recognised it as a string. 
+  Also, because the symbols are sparsely represented in unicode Numbers, I found it quite challenging to respresent them with unicode. 
+  
+I faced challenges in the loop the Random functions but recall that the random variable will all form a new variable together and used the math function += to resolve the issue I was facing.
+
+
+  
+
+   
+  
--- script.js
+++ script.js
@@ -5,100 +5,106 @@ var passwordText = document.querySelector("#password");
 
 
 // Write password to the #password input
-
- // used var instead of const. to allow for update. 
-    // the block bracket used for arrays did not allow for the symbols to 
-    //recognised as individual symbols rather it recognised it as a string. 
-    //Also, because the symbols are sparsely represented in unicode number; I found
-    //it quite challenging to respresent them with unicode. 
-
-    function getRandomSymbol() {
-      var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-      for (var i = 0; i < symbolSize; i++) {
-        return symbol[Math.floor(Math.random() * symbol.length)];
-      }
-    } console.log(getRandomSymbol());
-
-    //var LowerCase = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-    //I used the unicode number to represent the letters and the number of letters for its length.
-    function getRandomLowerCase() {
-      for (var i = 0; i < lowerCaseSize; i++) {
-        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-      }
-    }
-
-    //var UpperCase = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
-    //I used the unicode number to represent the letters and the number of letters for its length.
-    function getRandomUpperCase() {
-      for (var i = 0; i < upperCaseSize; i++) {
-        return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-      }
-    }
-
-    //var number = ["0,1,2,3,4,5,6,7,8,9"];
-    //I used the unicode number to represent the numbers and the total index for its length.
-    function getRandomNumber() {
-      for (var i = 0; i < numberSize; i++) {
-        return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-      }
-    }
-
 function generatePassword() {
   alert("select how many characters to be contained in your password.");
   var passwordSize = prompt("select between from 8 to 128");
-  if (passwordSize < 8 || passwordSize > 128 && isNaN(passwordSize) === true) {
+  if (passwordSize < 8 || passwordSize > 128 || isNaN(passwordSize) === true) {
     alert("pls try again"); return null;
   }
+  else { passwordSize; }
+
   var lowerCaseSize = parseInt(prompt("how many lowercase letters? "));
-  if (lowerCaseSize < 1) {
-    alert("pls try again "); return;
+  if (lowerCaseSize < 1 || (isNaN(lowerCaseSize) === true)) {
+    alert("pls try again "); return null;
   }
-  else if (isNaN(lowerCaseSize) === true) {
-    alert("pls try again "); return;
-  }
+  else { lowerCaseSize; }
+
   var upperCaseSize = parseInt(prompt("how many uppercase letters? "));
-  if (upperCaseSize < 1) {
-    alert("pls try again "); return;
+  if (upperCaseSize < 1 || (isNaN(upperCaseSize) === true)) {
+    alert("pls try again "); return null;
   }
-  else if (isNaN(upperCaseSize) === true) {
-    alert("pls try again "); return;
-  }
+  else { upperCaseSize; }
+
   var numberSize = parseInt(prompt("how many numbers? "));
-  if (numberSize < 1) {
-    alert("pls try again "); return;
+  if (numberSize < 1 || (isNaN(numberSize) === true)) {
+    alert("pls try again "); return null;
   }
-  else if (isNaN(numberSize) === true) {
-    alert("pls try again "); return;
-  }
+  else { numberSize; }
+
   var symbolSize = parseInt(prompt("how many symbols? "));
-  if (symbolSize < 1) {
-    alert("pls try again "); return;
+  if (symbolSize < 1 || (isNaN(symbolSize) === true)) {
+    alert("pls try again "); return null;
   }
-  else if (isNaN(symbolSize) === true) {
-    alert("pls try again "); return;
+  else { symbolSize; }
+
+  //var LowerCase = ["a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
+  //I used the unicode number to represent the letters and the number of letters for its length.
+
+  function getRandomLowerCase() {
+    var getLower = "";
+    for (var i = 0; i < lowerCaseSize; i++) {
+      var RandomLower = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+      getLower = RandomLower + getLower;
+    } return getLower;
+  } //console.log(getRandomLowerCase());
+
+  //var UpperCase = ["A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z"];
+  //I used the unicode number to represent the letters and the number of letters for its length.
+  function getRandomUpperCase() {
+    var getUpper = "";
+    for (var i = 0; i < upperCaseSize; i++) {
+      var RandomUpper = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+      getUpper = RandomUpper + getUpper;
+    } return getUpper;
+  } //console.log(getRandomUpperCase());
+
+  //var number = ["0,1,2,3,4,5,6,7,8,9"];
+  //I used the unicode number to represent the numbers and the total index for its length.
+  function getRandomNumber() {
+    var getNum = "";
+    for (var i = 0; i < numberSize; i++) {
+      RandomNum = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+      getNum = RandomNum + getNum;
+    } return getNum;
+  } //console.log(getRandomNumber());
+
+  // used var instead of const. to allow for update. 
+  // the block bracket used for arrays did not allow for the symbols to 
+  //recognised as individual symbols rather it recognised it as a string. 
+  //Also, because the symbols are sparsely represented in unicode number; I found
+  //it quite challenging to respresent them with unicode. 
+  function getRandomSymbol() {
+    var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
+    var getSymbol = "";
+    for (var i = 0; i < symbolSize; i++) {
+      var RandomSymbol = symbol[Math.floor(Math.random() * symbol.length)];
+      getSymbol = RandomSymbol + getSymbol;
+    } return getSymbol;
+  } //console.log(getRandomSymbol());
+
+  if (passwordSize = lowerCaseSize + upperCaseSize + numberSize + symbolSize) {
+    getRandomLowerCase();
+    getRandomUpperCase();
+    getRandomNumber();
+    getRandomSymbol();
+
+    return (getRandomSymbol().concat(getRandomLowerCase(), getRandomUpperCase(), getRandomNumber()));
+
   }
+  else { return; }
+} //console.log(generatePassword());
 
-  if (passwordSize === [lowerCaseSize + upperCaseSize + numberSize + symbolSize]) {
-
-   
-
-    getRandomSymbol().concat(getRandomLowerCase(), getRandomUpperCase(), getRandomNumber());
-  }
-  else { return null; }
-}
-console.log(generatePassword());
 
 function writePassword() {
 
   var password = generatePassword();
+  
   passwordText.value = password;
 
   document.getElementById("password").innerHTML = password;
 }
 
 
-
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
+
